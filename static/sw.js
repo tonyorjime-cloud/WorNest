539f4fb5174ab8b46da76ced87941a3ad0ff3c66
Tony <tony.orjime@gmail.com>
2026-02-20 10:12:39 +0100

    again

---
+++ static/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Minimal SW: we are not caching aggressively to avoid stale app issues.
